c76e38a356186b04508128543ad909b5dece9ecd
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-03-19 14:25:35 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import { sendDataToGA } from './utils/events';
 const min = 2000;
 const max = 3_000_000;
 
-const chips = [2000, 5000, 15000, 25000];
+const chips = [2000, 5000, 15000, 36000];
 
 type OptionKey = 'per_month' | 'per_week' | 'per_quarter' | 'per_annual';
 
